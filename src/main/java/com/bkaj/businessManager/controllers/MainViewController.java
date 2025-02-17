46229a7d1305240e6175bbc4f92c4d3436a9685f
bkajj <muuciek.new@gmail.com>
2025-02-17 17:02:13 +0100

    added basic mock layout

--- src/main/java/com/bkaj/businessManager/controllers/MainViewController.java
+++ src/main/java/com/bkaj/businessManager/controllers/MainViewController.java
@@ -1,4 +1,17 @@
 package com.bkaj.businessManager.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.GridPane;
+
 public class MainViewController {
+
+    @FXML
+    private GridPane mainGridPane;
+
+    @FXML
+    public void initialize() {
+
+    }
 }
